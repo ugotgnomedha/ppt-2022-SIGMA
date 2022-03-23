c4ade09676e6468ee10931e377269de7a71d3da0
ugotgnomedha <nick.sofa71@yandex.ru>
2022-03-23 09:20:09 +0300

    task N2

--- ppt-task-2/main.py
+++ ppt-task-2/main.py
@@ -1,1 +1,67 @@
-print("Never Gonna Give You Up!")
+import socket
+import json
+
+
+def trashResponseCheck(response):
+    if response == "EXIT 1":
+        exit(1)
+    elif response == "EXIT 2":
+        exit(2)
+
+
+def establishConn():
+    try:
+        sock.sendto(b'INIT_CIRC', (UDP_IP, UDP_PORT))
+        data = sock.recv(4096)
+        trashResponseCheck(data)  # Check if response is unclear.
+        if data == "OK":
+            print("Установлено подключение к контуру безопасности")
+            while stateChecker():  # Check server status.
+                i = 0  # Do nothing if state is normal
+            else:
+                print("Внимание, нарушение контура безопасности!")
+                stopper()  # Alarm
+    except socket.timeout:
+        print("Не удалось установить подключение")
+
+
+def stateChecker():
+    sock.sendto(b'CIRC_STATE', (UDP_IP, UDP_PORT))
+    data = sock.recv(4096)
+    trashResponseCheck(data)  # Check if response is unclear.
+    state = True
+    if data == "SOLID":
+        state = True
+    elif data == "BREAK":
+        state = False
+    return state
+
+
+def stopper():
+    sock.sendto(b'ALARM', (UDP_IP, UDP_PORT))
+    data = sock.recv(4096)
+    trashResponseCheck(data)  # Check if response is unclear.
+    if data != "STOPPED":
+        stopper()  # Send ALARM again if previous didn't stop the server.
+    elif data == "STOPPED":
+        stateDetails()  # Get detailed info about state.
+
+
+def stateDetails():
+    sock.sendto(b'CIRC_ALL_STATE', (UDP_IP, UDP_PORT))
+    data = sock.recv(4096)
+    trashResponseCheck(data)  # Check if response is unclear.
+    response = json.loads(data)
+    print(response)
+
+
+sock = socket.socket(socket.AF_INET,  # Internet
+                     socket.SOCK_DGRAM)  # UDP
+sock.settimeout(5)  # 5 second timeout on commands
+
+UDP_IP = "localhost"
+UDP_PORT = 5005
+
+establishConn()  # Establish connection to server.
+
+sock.close()
